f6d2c4a996a239cba315ec975aa91dd9c4bd4066
devsheder <adrien.lasselle@gmail.com>
2016-09-12 18:50:43 +0200

    Ajout fonctionnalite d'envoi de message vers le peripherique BT

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -32,26 +32,61 @@ function connexion() {
 }
 
 /**
- *
- * @param event
+ * Appel service READ du périphérique BT associé
  */
 function testServiceRead() {
     if (serverRpi) {
         // Récupération du service BT exposé
         serverRpi.getPrimaryService(uuidService).then(service => {
             // Appel à la charactéristique "READ" du service
-            service.getCharacteristic(uuidRead)
-            .then(characteristic => {
-            characteristic.readValue().then(value => {
-                appendHTML("testResult", "Valeur retournée par le service BT : " + new TextDecoder("utf-8").decode(value) + "<br/>");
-            }, error => {
-                appendHTML("error", "Erreur lors de l'appel au service : " + error + "<br/>");
-            });
-        })
-    }, error => {
+            service.getCharacteristic(uuidRead).then(characteristic => {
+                characteristic.readValue().then(value => {
+                    appendHTML("testResultRead", "Valeur retournée par le service BT : " + new TextDecoder("utf-8").decode(value) + "<br/>");
+                }, error => {
+                    appendHTML("error", "Erreur lors de l'appel au service : " + error + "<br/>");
+                });
+            })
+        }, error => {
             appendHTML("error", "Erreur lors de la récupération du service BT : " + error + "<br/>");
         });
     }
+}
+
+/**
+ * Appel service WRITE du périphérique BT associé
+ * @param event
+ */
+function testServiceWrite() {
+    if (serverRpi) {
+        // Récupération du service BT exposé
+        serverRpi.getPrimaryService(uuidService).then(service => {
+            // Appel à la charactéristique "READ" du service
+            var toSend = "Hello world !";
+            service.getCharacteristic(uuidRead).then(characteristic => {
+                characteristic.writeValue(str2ab(toSend))
+            }).then(test => {
+                appendHTML("testResultWrite", "Envoi du message ["+toSend+"] OK !<br/>");
+            }, error => {
+                appendHTML("testResultWrite", "Envoi du message ["+toSend+"] KO : " + error + "<br/>");
+            });
+        }, error => {
+            appendHTML("error", "Erreur lors de la récupération du service BT : " + error + "<br/>");
+        });
+    }
+}
+
+/**
+ *
+ * @param str
+ * @returns {ArrayBuffer}
+ */
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
 }
 
 /**
